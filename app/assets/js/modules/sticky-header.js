9d81b182a95bd9ab0bd2600e16ad408e1bd12272
dj51234 <dj51234@yahoo.com>
2017-04-25 19:07:07 -0400

    added modal functionality and smoothscrolling/follow-along links to header

--- app/assets/js/modules/sticky-header.js
+++ app/assets/js/modules/sticky-header.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class StickyHeader {
@@ -6,11 +7,51 @@ class StickyHeader {
     this.$nav = $('.header');
     this.$navLogo = $('.header__logo');
     this.$triggerElement = $('.large-hero__heading');
-    this.createWaypoint();
+    this.$pageSections = $('.section');
+    this.$navLinks = $('.primary-nav a');
+    this.createHeaderWaypoint();
+    this.createSectionWaypoints();
+    this.addSmoothScroll();
   }
 
-  createWaypoint() {
+  createSectionWaypoints() {
+
     const that = this;
+
+    this.$pageSections.map(function() {
+      const currentSection = this;
+
+      new Waypoint({
+        element: currentSection,
+        handler(direction) {
+          if (direction === 'down') {
+            const currentLink = currentSection.getAttribute('data-header-link');
+            that.$navLinks.removeClass('is-current-section');
+            $(currentLink).addClass('is-current-section');
+          }
+        },
+        offset: '18%'
+      });
+
+      new Waypoint({
+        element: currentSection,
+        handler(direction) {
+          if (direction === 'up') {
+            const currentLink = currentSection.getAttribute('data-header-link');
+            that.$navLinks.removeClass('is-current-section');
+            $(currentLink).addClass('is-current-section');
+          }
+        },
+        offset: '-40%'
+      });
+    });
+
+  }
+
+  createHeaderWaypoint() {
+
+    const that = this;
+
     new Waypoint({
       element: that.$triggerElement[0],
       handler: function(direction) {
@@ -23,11 +64,12 @@ class StickyHeader {
         )
       }
     });
+
   }
 
-  // When nav gets to certain point on window
-    // Trigger css class at a spot on the window
-    // Change bg to darker color
+  addSmoothScroll() {
+    this.$navLinks.smoothScroll();
+  }
 }
 
 export default StickyHeader;
