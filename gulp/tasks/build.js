d5283826d4152d878c44558b1c29352837fc6d66
dj51234 <dj51234@yahoo.com>
2017-04-25 23:50:19 -0400

    added gulp build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const usemin = require('gulp-usemin');
+
+gulp.task('deleteDistFolder', function() {
+  return del('./dist');
+});
+
+gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+               .pipe(imagemin({
+                 progressive: true,
+                 interlaced: true,
+                 multipass: true
+               }))
+               .pipe(gulp.dest('./dist/assets/images'));
+});
+
+gulp.task('usemin', ['deleteDistFolder'], function() {
+  return gulp.src('./app/index.html')
+             .pipe(usemin())
+             .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('build', ['deleteDistFolder','optimizeImages', 'usemin'], function() {
+
+});
